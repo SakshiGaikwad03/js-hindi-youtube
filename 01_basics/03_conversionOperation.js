f90d445e1eb5f847fa25d90ca5f7e319f9891040
Sakshi Gaikwad <sakshigaik9876@gmail.com>
2025-07-03 10:08:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score =  "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = "kkkl"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+//1 => true; 0 => false
+// "" => false
+// "frg" => true
+
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
