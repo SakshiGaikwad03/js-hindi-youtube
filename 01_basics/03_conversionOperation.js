3910114b1d07ee0529dfd4dc95b9d8de608ef2d4
Sakshi Gaikwad <sakshigaik9876@gmail.com>
2025-07-03 11:06:37 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,24 +1,47 @@
-let score =  "33abc"
+// let score =  "33abc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 let isLoggedIn = "kkkl"
 
-let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
 
 //1 => true; 0 => false
 // "" => false
 // "frg" => true
 
 
-let someNumber = 22
+// let someNumber = 22
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+
+
+
+// *************************  Operations ***************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let num1, num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+//link to study
+//https://tc39.es/ecma262/#sec-number-objects
+
