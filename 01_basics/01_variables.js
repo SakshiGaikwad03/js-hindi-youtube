0fc661b8962ba9d7a539fcdda639cd602c4c1c99
Sakshi Gaikwad <sakshigaik9876@gmail.com>
2025-07-03 08:16:31 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "sakshi@gmail.com"
+var accountPassword = "12345"
+accountCity = "nashik"
+
+let accountState;
+
+
+
+console.log(accountId);
+
+
+console.table([accountEmail,accountPassword,accountCity,accountId,accountState]);
