0f3e049004b94af2b9c5b963617107f53c5ded14
Sakshi Gaikwad <sakshigaik9876@gmail.com>
2025-07-08 06:29:31 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,5 +3,16 @@ const repoCount = 3
 
 //  console.log(name + repoCount + "value");
 
-console.log('hello ${name} and my repocount ${repoCount}');
+console.log(`hello ${name} and my repocount ${repoCount}`);
+
+const gameName = new String('sakshiii');
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
 
