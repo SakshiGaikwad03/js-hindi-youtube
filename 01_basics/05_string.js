a119b2ce5b2f244c669dbfb9c32be40d6ce71640
Sakshi Gaikwad <sakshigaik9876@gmail.com>
2025-07-09 10:44:26 +0000

    string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -11,8 +11,20 @@ const gameName = new String('sakshiii');
 // console.log(gameName.__proto__);
 
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-5,4)
+console.log(anotherString);
 
 
+const url = "https://www.google.com?user=sakshi&id=1234&age=20";
+
+comsole.log(url.replace('%20', '-'))
 
